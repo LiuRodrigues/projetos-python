241958f9d587dc906fbd4bb321028f5a71c11aff
Liu Rodrigues <80280496+LiuRodrigues@users.noreply.github.com>
2021-03-09 12:35:06 -0300

    finalizado

--- validCpf.py
+++ validCpf.py
@@ -1,19 +1,22 @@
 # validadar cpf
 def validCpf(dig):
-    cpf_recebido = dig
-    cpf_gerado = cpf_recebido[0:9]
+    cpf_recebido = str(dig)
+    cpf_gerado = cpf_recebido[0:-2]
     cont_regres = 10
     totalizador = 0
     for index in range(18):
         if index > 8:
             index -= 8
-        totalizador += (cont_regres * int(cpf_recebido[index]))
+        totalizador += (cont_regres * int(cpf_gerado[index]))
         cont_regres -= 1
         if cont_regres < 2:
-            cont_regres = 10
             d = 11 - (totalizador % 11)
             if d > 9:
                 d = 0
-            cpf_gerado += str(d)
             totalizador = 0
-            return cpf_gerado
+            cont_regres = 10
+            cpf_gerado += str(d)
+    return cpf_gerado == cpf_recebido
+
+
+print(validCpf(47756855205))
