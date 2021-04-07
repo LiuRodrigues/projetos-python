c92f1378a93c908c6185a4fa2dafafde6b93fb09
Liu (srSouzaFilho) <80280496+LiuRodrigues@users.noreply.github.com>
2021-04-07 16:07:47 -0300

    Update lista.py

--- lista_compras/lista.py
+++ lista_compras/lista.py
@@ -2,12 +2,15 @@
 from cabeçalho import cab_linhas, lin
 from useful_formatting import brl, texto_tabulado
 
+
 produtos, c = [], 0
-while c < 1:
-    produtos.append(texto_tabulado(input('Produto: ').upper(), float(input('Preço: '))))
-    c += 1
+while c == 0:
+    produtos.append(texto_tabulado(input('Produto: ').upper(), float(input('Quantidade: '))))
+    print('Escolha: [0] Para continuar [1] Para sair')
+    c = int(input('Quer continuar?'))
+
 titulo = cab_linhas('LISTAGEM DE COMPRAS', '=')
-print(f'{titulo}\n{"COD"} {"PRODUTO"} {"PREÇO": >25}')
+print(f'{titulo}\n{"COD"} {"PRODUTO"} {"QTD": >25}')
 
 for pos, nome in enumerate(produtos):
     print(f'{pos + 1:0>3}', nome)
@@ -15,7 +18,7 @@ print(lin('-', 38))
 
 with open('lista.txt', 'w') as arquivo:
     arquivo.write(str(cab_linhas('LISTAGEM DE COMPRAS', '=')))
-    arquivo.write('\n{} {}{: >25}'.format('CÓD', 'PRODUTO', 'PREÇO') + '\n')
+    arquivo.write('\n{} {}{: >25}'.format('COD', 'PRODUTO', 'QTD') + '\n')
     arquivo.write(lin('-', 38) + '\n')
 
     for pos, valor in enumerate(produtos):
