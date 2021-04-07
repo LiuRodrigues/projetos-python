be984178bb1b53437c150a3f1d6baf3c2c67d815
Liu (srSouzaFilho) <80280496+LiuRodrigues@users.noreply.github.com>
2021-04-07 16:08:30 -0300

    Update useful_formatting.py

--- lista_compras/useful_formatting.py
+++ lista_compras/useful_formatting.py
@@ -1,13 +1,22 @@
 # Formatação tipo moeda => Exemplo: "R$ 3.00"
 def brl(num):
     text = f'R$ ' + str(f'{num:.2f}')
+
+
+def qtd(num):
+    text = str(f'{num:.2f}')
     return text
 
 
-#  Exemplo: "Produto ...............R$ 3.00"
+    #  Exemplo: "Produto ...............R$ 3.00"
 def texto_tabulado(txt, num):
     texto = txt
-    linha = f'{texto}{"":.>{26-len(texto)}}{brl(num)}'
+    linha = f'{texto}{"":.>{30-len(texto)}}{qtd(num)}'
     return linha
+
+
+
+
+
 
 
